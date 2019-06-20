d4482e59c18e7289671377a7d642836ff6ce0844
Kieran Gibb <kieran@tenthousandthings.org.uk>
2019-06-20 16:58:10 +0100

    view

--- view.js
+++ view.js
@@ -2,31 +2,38 @@ var through2 = require('through2')
 var collect = require('collect-stream')
 const { EventEmitter } = require('events')
 
+const MESSAGE_TYPE = 'list/item'
+const NAMESPACE = 'item!'
+
 module.exports = function View (db, opts) {
   const events = new EventEmitter()
 
+  // This view creates an index of all items,
+  // stores them in a sublevel namespaced first with 'item!'
+  // and orders first by name, then by timestamp
+
   const putItem = (msg) => ({
     type: 'put',
-    key: ['item!', msg.value.content.name, '!', msg.value.timestamp].join(''),
+    key: [NAMESPACE, msg.value.content.name, '!', msg.value.timestamp].join(''),
     value: [msg.key, '@', msg.seq].join('')
   })
 
   const getItem = (msg) => ({
-    key: ['item!', msg.value.content.name, '!', msg.value.timestamp].join(''),
+    key: [NAMESPACE, msg.value.content.name, '!', msg.value.timestamp].join(''),
     value: [msg.key, '@', msg.seq].join('')
   })
 
   return {
     map: function map (msgs, next) {
       var mapped = msgs
-        .filter((msg) => msg.value.type === 'list/item')
+        .filter((msg) => msg.value.type === MESSAGE_TYPE)
         .map(putItem)
 
       db.batch(mapped, next)
     },
     indexed: function indexed (msgs) {
       msgs
-        .filter((msg) => msg.value.type === 'list/item')
+        .filter((msg) => msg.value.type === MESSAGE_TYPE)
         .map(getItem)
         .forEach((msg) => events.emit('insert', msg))
     },
